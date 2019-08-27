fcc536d873544b6467b3e8166aba9ab66c90eada
Jacob Paris <jacob@jacobpariseau.com>
2019-08-26 23:10:49 -0600

    Add logout button

--- app/components/header.js
+++ app/components/header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
+import { Button } from "../components/button";
+
 export const Container = styled.div`
     display: flex;
     justify-content: center;
@@ -11,10 +13,35 @@ export const HeaderText = styled.h1`
     margin: 0.25rem 0;
 `;
 
+export const HeaderActions = styled.div`
+    position: absolute;
+    right: 1rem;
+    top: 0.25rem;
+    bottom: 0;
+`; 
+
+export function SignoutButton() {
+    const signOut = React.useCallback(() => useAuth().signOut().then(() => {
+        // Refresh after signout
+        location.reload();
+    }), []);
+
+    return (
+        <Button inverted onClick={signOut}>Sign Out</Button>
+    );
+}
+
 export function Header() {
+    
     return (
         <Container>
             <HeaderText> 📺 Talk TV </HeaderText>
+            <HeaderActions>
+                <SignoutButton />
+            </HeaderActions>
         </Container>
     )
+}
+function useAuth() {
+    return gapi.auth2.getAuthInstance();
 }
