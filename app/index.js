09f6ebac546ecf309a6991ec555e76d1f489748e
Jacob Paris <jacob@jacobpariseau.com>
2019-08-26 22:49:26 -0600

    Check Logged in Status

--- app/index.js
+++ app/index.js
@@ -10,11 +10,32 @@ const API_KEY = process.env.API_KEY;
 const YOUTUBE_DISCOVERY_URI = "https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest";
 
 function App() {
-    const isSignedIn = false;
+    const isSignedIn = gapi.auth2.getAuthInstance().isSignedIn.get();
 
     return isSignedIn ? (
         <h1> 📺 Talk TV</h1>
     ) : <LoginScene />;
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function AppContainer() {
+    const wasGoogleReady = !!gapi.auth2;
+    const [isGoogleReady, setGoogleReady] = React.useState(wasGoogleReady);
+
+    React.useEffect(() =>{
+        gapi.load('client:auth2', () => {
+            const authPromise = gapi.auth2.init({
+                clientId: CLIENT_ID,
+            });
+            
+            gapi.client.setApiKey(API_KEY);
+            const youtubePromise = gapi.client.load(YOUTUBE_DISCOVERY_URI);
+            
+            Promise.all([authPromise, youtubePromise]).then(() => setGoogleReady(true));
+        })
+    }, [isGoogleReady]);
+    
+    return isGoogleReady ? <App /> : <div>Loading...</div>;
+}
+
+
+ReactDOM.render(<AppContainer />, document.getElementById("root"));
