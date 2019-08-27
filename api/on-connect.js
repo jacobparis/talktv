9579642b737acfb3101aec218efbd0bb79d3117a
Jacob Paris <jacob@jacobpariseau.com>
2019-08-27 01:34:23 -0600

    Store socket id in table during connection

--- api/on-connect.js
+++ api/on-connect.js
@@ -1,7 +1,21 @@
 const AWS = require('aws-sdk');
 
 exports.handler = async (event, context) => {
+    const dynamoDB = new AWS.DynamoDB.DocumentClient((process.env.IS_LOCAL || process.env.IS_OFFLINE) ? {
+        region: "localhost",
+        endpoint: `http://localhost:${process.env.PORT || 8000}`
+    } : {});
+
     const id = event.requestContext.connectionId;
 
     console.log("👀  ", `Opened connection: ${id}`);
+
+    const openParams = {
+        TableName:`${process.env.PREFIX}-connections`,
+        Item: {
+            ConnectionId: id
+        }
+    }
+
+    return dynamoDB.put(openParams).promise();
 };
--- api/on-disconnect.js
+++ api/on-disconnect.js
@@ -1,7 +1,21 @@
 const AWS = require('aws-sdk');
 
 exports.handler = async (event, context) => {
+    const dynamoDB = new AWS.DynamoDB.DocumentClient((process.env.IS_LOCAL || process.env.IS_OFFLINE) ? {
+        region: "localhost",
+        endpoint: `http://localhost:${process.env.PORT || 8000}`
+    } : {});
+
     const id = event.requestContext.connectionId;
 
     console.log("👀  ", `Closed connection: ${id}`);
+
+    const deleteParams = {
+        TableName:`${process.env.PREFIX}-connections`,
+        Key: {
+            ConnectionId: id
+        }
+    };
+
+    return dynamoDB.delete(deleteParams).promise();
 };
