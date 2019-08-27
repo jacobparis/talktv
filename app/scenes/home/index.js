b83028a0cf303f8ce526c79757e04df6ee9e9507
Jacob Paris <jacob@jacobpariseau.com>
2019-08-26 23:54:04 -0600

    Request List o' videos from youtube api

--- app/scenes/home/index.js
+++ app/scenes/home/index.js
@@ -3,10 +3,47 @@ import React from "react";
 import { Header } from "../../components/header";
 
 export default function() {
+
+    React.useEffect(() => {
+        let isSubscribed = true;
+
+        getVideos().then(videos => {
+            if (isSubscribed) {
+                console.log(videos);
+            }
+        });
+
+        return () => isSubscribed = false;
+    }, []);
+
     return (
         <div>
             <Header />
             <h1> Welcome home, logged in user!</h1>
         </div>
     );
+}
+
+function getVideos() {
+    return new Promise((resolve, reject) => {
+        gapi.client.youtube.search.list({
+            "part": "snippet",
+            "eventType": "live",
+            "maxResults": 12,
+            "q": "game",
+            "type": "video"
+        }).then(response => {
+            console.log("GET VIDEOS", response);
+            const items = response.result.items;
+
+            if(items) {
+                resolve(items);
+            } else {
+                reject();
+            }
+        }).catch(error => {
+            console.log("ERROR VIDEOS", error);
+            reject();
+        });
+    });
 }
